5013674aa9bd017c7883873d03f2cbeb107dd322
Mads Akselsen <akselsenmads@gmail.com>
2022-04-05 08:33:13 +0200

    sorting

--- card.go
+++ card.go
@@ -2,7 +2,10 @@
 
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Suit uint8
 
@@ -52,10 +55,16 @@ func (c Card) String() string {
 	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
 }
 
-type Deck []Deck
+type NewOpts struct {
+	Shuffle bool
+}
 
-// Create new deck of cards
-func New() []Card {
+// Create new deck of cards.
+// Cards are ordered by default, but can pass in an
+// array of functions to further manipulate the deck.
+// Each function takes the deck as argument, and returns
+// a new changed deck.
+func New(options ...func([]Card) []Card) []Card {
 	var cards []Card
 	
 	for _, suit := range suits {
@@ -64,5 +73,44 @@ func New() []Card {
 		}
 	}
 
+	for _, option := range options {
+		cards = option(cards)
+	}
+
 	return cards
+}
+
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+// argument is: func(cards []Card) func(i, j int) bool, which is a less function
+// it takes in a slice of cards and returns a Less function.
+// It works like the above DefaultSort, but with the difference
+// that we pass in a custom less function
+func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+// Creates a unique rank for a card based on its Suit and rank.
+func absRank(c Card) int {
+	// The 'suit * maxRank' makes sure that Suits are
+	// not overlapping.
+	// For example,
+	// Spade will be 0*13=0
+	// Diamond will be 1*13=13
+	// Club will be 2*13=26
+	// So they cannot overlap because they each have unique range
+	//			1		*		13		+	4
+	return int(c.Suit) * int(maxRank) + int(c.Rank)
 }
